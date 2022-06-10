2b0ffe7336455067930a99819d01d7d917ac1cde
Kevin <programador.kevin4773@gmail.com>
2022-06-10 16:50:09 -0300

    Projeto pokedex

---
+++ app.js
@@ -0,0 +1,39 @@
+const fetchPokemon = () => {
+
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+    const pokemonPromises = []
+
+    for (let i = 1; i <= 150; i++) {
+        
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));
+        
+    }
+
+    Promise.all(pokemonPromises)
+    .then(pokemons => {
+
+        const lisPokemons = pokemons.reduce((acc, pokemon) => {
+
+            const types = pokemon.types.map(TypeInfo => TypeInfo.type.name);
+
+            acc += `
+                <li class="card ${types[0]}">
+                    <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
+                    <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                    <p class="card-subtitle">${types.join(" | ")}</p>
+                </li>
+            `;
+            
+            return acc;
+        }, "");
+
+        const ul = document.querySelector("[data-js='pokedex']");
+
+        ul.innerHTML = lisPokemons;
+
+    });
+
+}
+
+fetchPokemon();
